129ef191931ea8239e7486eb14ab00f789ca7586
brentgroves <brent.groves@gmail.com>
2023-11-27 17:12:22 -0500

    updated source code

--- trlbal/run_script.go
+++ trlbal/run_script.go
@@ -11,10 +11,10 @@ import (
 func RunScript(script string) string {
 	// fmt.Printf("Path Separator: '%c'\n", os.PathSeparator)
 
-	fmt.Printf("/home/brent/src/reports/volume/go/runner/main\n")
+	fmt.Printf("/home/brent/src/repsys/volumes/go/runner/main\n")
 	//  ./test.sh dev reports11 30011 1 reports11 30311 reports
 	// /home/brent/src/reports/volume/go/runner/main/test.sh dev reports11 30011 1 reports11 30311 reports
-	cmd6 := exec.Command("/home/brent/src/reports/volume/go/runner/main/test.sh", "dev", "reports11", "30011", "1", "reports11", "30311", "reports")
+	cmd6 := exec.Command("/home/brent/src/repsys/volumes/go/runner/main/test.sh", "dev", "reports11", "30011", "1", "reports11", "30311", "reports")
 	cmd6.Stdout = os.Stdout
 
 	err6 := cmd6.Run()
